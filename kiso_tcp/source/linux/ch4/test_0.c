6faa157231ad11514129d772b1d5aefbca7dd0fb
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-05-14 06:29:22 +0900

    fix program to work on linux

---
+++ kiso_tcp/source/linux/ch4/test_0.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{
+    puts("--start--");
+
+    #ifdef __unix
+    puts("unix");
+    #endif
+
+    #ifdef __linux
+    puts("linux");
+    #endif
+
+    #ifndef __linux
+    puts("not linux");
+    #endif
+
+    #ifdef __GNUC__
+    puts("GNUC");
+    #endif
+
+    #ifdef __sun
+    puts("sun");
+    #endif
+
+    #ifdef __sparc
+    puts("sparc");
+    #endif
+
+    #ifdef __solaris
+    puts("solaris");
+    #endif
+
+    puts("--end--");
+    return 0;
+}

e7e2a05fb800c866d4f198a2258986dcf01197d6
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-05-17 06:40:37 +0900

    Once

---
+++ kiso_tcp/kiwata/ch3/udpc.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+#define DEFAULT_PORT 5320
+
+void die(char *msg);
+
+void die(char *msg) {
+    perror(msg);
+    exit(EXIT_FAILURE);
+}
+
+// socket() -> sendto -> recvfrom の流れで実装していく。
+int main(int argc, char **argv) {
+    int port = DEFAULT_PORT;
+    char *address = "192.168.11.16";
+
+    // 人間が読みやすい IP アドレスからネットワークバイトオーダに変換する。
+    in_addr_t dest_ip;
+    if ((dest_ip = inet_addr(address)) == INADDR_NONE) {
+        struct hostent *he;
+        he = gethostbyname(address);
+    }
+
+    int s = socket(AF_INET, SOCK_DGRAM, 0);
+    if (s < 0) {
+        die("socket");
+    }
+
+    while (true) {
+        // bind は必要ない。
+        // 理由は、socket にサーバーのアドレスやポート番号などの情報を紐付けるからである。
+
+        // man で確認
+        // ssize_t sendto(
+        //     int sockfd,
+        //     const void *buf,
+        //     size_t len,
+        //     int flags,
+        //     const struct sockaddr *dest_addr,
+        //     socklen_t addrlen
+        // )
+        // /usr/include/netinet/in.h を確認する。
+        // struct sockaddr_in {
+        //     sa_family_t sin_family;
+        //     in_port_t sin_port;
+        //     struct in_addr sin_addr;
+        //     // padding のメンバもある。
+        // }
+        struct sockaddr_in server;
+        memset(&server, 0, sizeof(struct sockaddr_in));
+        server.sin_family = AF_INET;
+        server.sin_addr.s_addr = dest_ip;
+        server.sin_port = port;
+
+        char *buf = "Hello";
+        size_t len = strlen(buf);
+        // sendto の第五引数のキャストを忘れずに。
+        if (sendto(s, buf, len, 0, (struct sockaddr *) &server, sizeof(server)) < 0) {
+            die("sendto");
+        }
+
+        // man で確認
+        // ssize_t recvfrom(
+        //     int sockfd,
+        //     void *buf,
+        //     size_t len,
+        //     int flags,
+        //     struct sockaddr *src_addr,
+        //     socklen_t *addrlen
+        // )
+        // src_addr が NULL 以外で、下層のプロトコルからメッセージの送信元アドレスがわかる場合、この送信元アドレスが src_addr が指すバッファに格納される。
+        // この場合、addrlen は入出力両方の引数となる。
+        // 呼び出し前に、呼び出し元は src_addr に割り当てたバッファで初期化しておくべきである。
+        // 呼び出し元が送信アドレスを必要としない場合には、src_addr と addrlen には NULL を指定するべきである。
+        ssize_t n;
+        char *recv_buf;
+        if ((n = recvfrom(s, recv_buf, strlen(recv_buf), 0, NULL, NULL)) < 0) {
+            die("recvfrom");
+        }
+    }
+    close(s);
+
+    return 0;
+}
---
+++ kiso_tcp/kiwata/ch3/udps.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+// socket() -> bind() -> recvfrom() -> sendto() の流れで実装していく。
+int main(int argc, char **argv) {
+    return 0;
+}

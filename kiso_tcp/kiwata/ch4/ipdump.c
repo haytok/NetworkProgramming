0c0a4b4d55ebb47f7e692c1460dcec6dc3b7e7be
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-05-22 19:50:22 +0900

    crate packet capture

--- kiso_tcp/kiwata/ch4/ipdump.c
+++ kiso_tcp/kiwata/ch4/ipdump.c
@@ -25,7 +25,9 @@ char *ip_ftoa(int flag);
 void print_ip(struct ip *ip);
 void print_icmp(struct icmp *icmp);
 void print_udp(struct udphdr *udp);
+char *tcp_ftoa(int flag);
 void print_tcp(struct  tcphdr *tcp);
+void print_tcp_mini(struct tcphdr *tcp);
 char *mac_ntoa(uint8_t *host);
 
 int main(int argc, char **argv) {
@@ -88,7 +90,7 @@ int main(int argc, char **argv) {
         } else if (ntohs(eth->ether_type) == ETHERTYPE_IP) {
             // print_ip(ip);
             if (ip->ip_p == IPPROTO_TCP) {
-                print_tcp(tcp);
+                // print_tcp(tcp);
             } else if (ip->ip_p == IPPROTO_UDP) {
                 // print_udp(udp);
             } else if (ip->ip_p == IPPROTO_ICMP) {
@@ -196,21 +198,110 @@ void print_ip(struct ip *ip) {
 }
 
 void print_icmp(struct icmp *icmp) {
+    static char *type_name[] = {
+        "Echo Reply",               /* Type  0 */
+        "Undefine",                 /* Type  1 */
+        "Undefine",                 /* Type  2 */
+        "Destination Unreachable",  /* Type  3 */
+        "Source Quench",            /* Type  4 */
+        "Redirect (change route)",  /* Type  5 */
+        "Undefine",                 /* Type  6 */
+        "Undefine",                 /* Type  7 */
+        "Echo Request",             /* Type  8 */
+        "Undefine",                 /* Type  9 */
+        "Undefine",                 /* Type 10 */
+        "Time Exceeded",            /* Type 11 */
+        "Parameter Problem",        /* Type 12 */
+        "Timestamp Request",        /* Type 13 */
+        "Timestamp Reply",          /* Type 14 */
+        "Information Request",      /* Type 15 */
+        "Information Reply",        /* Type 16 */
+        "Address Mask Request",     /* Type 17 */
+        "Address Mask Reply",       /* Type 18 */
+        "Unknown"                   /* Type 19 */
+    };                            /* ICMPのタイプを表す文字列 */
+    #define ICMP_TYPE_MAX (sizeof type_name / sizeof type_name[0])
+    int type = icmp->icmp_type;
+    if (type < 0 || type >= ICMP_TYPE_MAX) {
+        type = ICMP_TYPE_MAX - 1;
+    }
+
     puts("Protocol: ICMP");
+    printf("+-------------------------+-------------------------+\n");
+    printf("| Type %3u | Code %3u | Checksum %5u |\n", icmp->icmp_type, icmp->icmp_code, ntohs(icmp->icmp_cksum));
+
+    if (type == ICMP_ECHOREPLY) {
+        printf("| ID  %u | Sequence Number %u | \n", ntohs(icmp->icmp_id), ntohs(icmp->icmp_seq));
+    } else if (type == ICMP_DEST_UNREACH) {
+        if (icmp->icmp_code == ICMP_UNREACH_NEEDFRAG) {
+            printf("| void  %u | Next MTU %u | \n", ntohs(icmp->icmp_pmvoid), ntohs(icmp->icmp_nextmtu));
+        } else {
+            printf("| void  %u | \n", ntohs(icmp->icmp_pmvoid));
+        }
+    } else if (type == ICMP_REDIRECT) {
+        printf("| Router IP Address %s |\n", inet_ntoa(*(struct in_addr *) &icmp->icmp_gwaddr));
+    } else if (type == ICMP_TIME_EXCEEDED) {
+        printf("| void  %u | \n", ntohs(icmp->icmp_pmvoid));
+    }
+
+    if (type == ICMP_DEST_UNREACH || type == ICMP_REDIRECT || type == ICMP_TIME_EXCEEDED) {
+        struct ip *ip = (struct ip *) icmp->icmp_data;
+        char *head = (char *) ip + ((int) (ip->ip_hl) << 2);
+
+        print_ip(ip);
+        switch (ip->ip_p) {
+            case IPPROTO_TCP:
+                print_tcp_mini((struct tcphdr *) head);
+                break;
+            case IPPROTO_UDP:
+                print_udp((struct udphdr *) head);;
+                break;
+        }
+    }
 }
 
 void print_udp(struct udphdr *udp) {
     puts("Protocol: UDP");
     printf("+-------------------------+-------------------------+\n");
     printf("| Source Port %5u | Dest Port %5u |\n", ntohs(udp->uh_sport), ntohs(udp->uh_dport));
-    printf("| Length %5u | Checksum %5u |\n", ntohs(udp->uh_ulen), ntohs(udp->uh_ulen));
+    printf("| Length %5u | Checksum %5u |\n", ntohs(udp->uh_ulen), ntohs(udp->uh_sum));
     printf("+-------------------------+-------------------------+\n");
+}
+
+char *tcp_ftoa(int flag) {
+    static int f[] = {'U', 'A', 'P', 'R', 'S', 'F'};
+#define TOS_MAX (sizeof f / sizeof f[0])
+    static char str[TOS_MAX + 1];
+    unsigned int mask = 1 << (TOS_MAX - 1);
+    int i;
+
+    for (i = 0; i < TOS_MAX; i++) {
+        if ((flag << i) & mask) {
+            str[i] = f[i];
+        } else {
+            str[i] = '0';
+        }
+    }
+    str[i] = '\0';
+
+    return str;
 }
 
 void print_tcp(struct  tcphdr *tcp) {
     puts("Protocol: TCP");
     printf("+-------------------------+-------------------------+\n");
-    printf("||\n");
+    printf("| Source Port %5u | Dest Port %5u |\n", ntohs(tcp->th_sport), ntohs(tcp->th_dport));
+    printf("| Sequence Number %10lu |\n", (unsigned long)ntohl(tcp->th_seq));
+    printf("| Acknowledgement Number %10lu |\n", (unsigned long)ntohl(tcp->th_ack));
+    printf("| DO %5u | Reserved | F %6s | Window %5u |\n", tcp->th_off, tcp_ftoa(tcp->th_flags), ntohs(tcp->window));
+    printf("| Checksum %5u | Urgent Pointer %5u |\n", ntohs(tcp->check), ntohs(tcp->urg_ptr));
+    printf("+-------------------------+-------------------------+\n");
+}
+
+void print_tcp_mini(struct tcphdr *tcp) {
+    puts("Protocol: TCP");
+    printf("| Source Port %5u | Dest Port %5u |\n", ntohs(tcp->th_sport), ntohs(tcp->th_dport));
+    printf("| Sequence Number %10lu |\n", (unsigned long)ntohl(tcp->th_seq));
     printf("+-------------------------+-------------------------+\n");
 }
 
